8f54d7c5d609d41c5078809869ef2ce93ad107f2
indexzero <charlie.robbins@gmail.com>
2012-02-20 12:10:58 -0500

    [api test] Finish work on `quill keys *`

--- test/commands/keys-test.js
+++ test/commands/keys-test.js
@@ -10,9 +10,35 @@ var assert = require('assert'),
     path = require('path'),
     nock = require('nock'),
     vows = require('vows'),
-    macros = require('../helpers/macros');
+    macros = require('../helpers/macros'),
+    quill = require('../../lib/quill');
 
 var shouldQuillOk = macros.shouldQuillOk;
+
+var keys = [
+  '0123456789', 
+  '1234567890',
+  '2345678991'
+];
+
+var appendKeys = [
+  'abcdefghij',
+  'klmnopqrst'
+];
+
+function assertWroteKeys(target) {
+  return function (_, err) {
+    assert.isTrue(!err);
+    
+    var written = fs.readFileSync(quill.common.authorizedKeys.filename, 'utf8')
+      .split('\n')
+      .filter(Boolean);
+      
+    target.forEach(function (key) {
+      assert.notEqual(written.indexOf(key), -1);
+    });
+  }
+}
 
 vows.describe('quill/commands/keys').addBatch({
   'keys hostname': shouldQuillOk(
@@ -27,6 +53,28 @@ vows.describe('quill/commands/keys').addBatch({
     function (err, hostname) {
       assert.isNull(err);
       assert.isString(hostname);
+    }
+  )
+}).addBatch({
+  'keys downloadall': shouldQuillOk(
+    'should write the correct keys to disk',
+    assertWroteKeys(keys),
+    function setup() {
+      quill.prompt.override['yesno'] = 'y';
+    
+      nock('http://api.testquill.com')
+        .get('/keys')
+        .reply(200, { keys: keys })
+    }
+  )
+}).addBatch({
+  'keys download devjitsu': shouldQuillOk(
+    'should append the correct keys to disk',
+    assertWroteKeys(keys.concat(appendKeys)),
+    function setup() {
+      nock('http://api.testquill.com')
+        .get('/keys/devjitsu')
+        .reply(200, { keys: appendKeys })
     }
   )
 }).export(module);
