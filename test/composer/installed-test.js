ce4b111b7ecd36ddbb5156b21fa131c9d7586014
indexzero <charlie.robbins@gmail.com>
2012-03-10 21:47:47 -0500

    [api test] Added full lifecycle (add, list, remove) for installed systems with associated tests.

--- test/composer/installed-test.js
+++ test/composer/installed-test.js
@@ -18,11 +18,55 @@ var assert = require('assert'),
 
 var fixturesDir = path.join(__dirname, '..', 'fixtures'),
     systemsDir = path.join(fixturesDir, 'systems'),
-    installDir = path.join(fixturesDir, 'install'),
+    installDir = path.join(fixturesDir, 'installed'),
     sourceDir = path.join(systemsDir, 'tgz');
 
 vows.describe('quill/composer/installed').addBatch(
   macros.shouldInit(function () {
     quill.config.set('directories:install', installDir);
   })
-)
+).addBatch({
+  "When using quill.composer.installed": {
+    "the addOne() method": {
+      topic: function () {
+        quill.composer.installed.addOne({
+          name: 'fixture-one',
+          version: '0.0.0',
+          path: path.join(systemsDir, 'fixture-one')
+        }, this.callback);
+      },
+      "should add the system to the install directory": function (err, system) {
+        assert.isNull(err);
+        assert.isString(system.path);
+        assert.isTrue(path.existsSync(system.path));
+        assert.isTrue(path.existsSync(path.join(system.path, 'system.json')));
+      }
+    }
+  }
+}).addBatch({
+  "When using quill.composer.installed": {
+    "the list() method": {
+      topic: function () {
+        quill.composer.installed.list(this.callback)
+      },
+      "should respond with the list of installed systems": function (err, systems) {
+        assert.isNull(err);
+        assert.isObject(systems);
+        assert.isObject(systems['fixture-one']);
+      }
+    }
+  }
+}).addBatch({
+  "When using quill.composer.installed": {
+    "the remove() method": {
+      topic: function () {
+        quill.composer.installed.remove('fixture-one', this.callback);
+      },
+      "should remove the specified system": function (err) {
+        assert.isTrue(!err);
+        assert.isTrue(path.existsSync(path.join(installDir, 'fixture-one')));
+        assert.isFalse(path.existsSync(path.join(installDir, 'fixture-one', '0.0.0')));
+      }
+    }
+  }
+}).export(module);
